73d45cd905d0b4dbd3929d950c077930525f55a7
Ameya Joshi <52293882+ameyamjoshi@users.noreply.github.com>
2020-03-07 15:31:47 +0530

    Add files via upload
    Download youtube videos for free

---
+++ Youtube_Downloader.py
@@ -0,0 +1,31 @@
+from pytube import YouTube
+from tkinter import *
+
+def Download():
+    link=a.get()
+    path=b.get()
+    yt=YouTube(link)
+    yt.title
+    stream = yt.streams.first()
+    stream.download(path)
+    return"Done"
+
+
+# a=input()
+# b=input()
+# Download(a,b)
+main=Tk()
+main.title("Downloader")
+main.geometry('300x300')
+a=StringVar()
+b=StringVar()
+lblAmount = Label(main, text = 'Link:').grid(row = 0, column = 0, padx = 30, pady = 10)
+entAmount = Entry(main, textvariable = a).grid(row = 0, column = 1)
+
+lblIntRate = Label(main, text = 'Path:').grid(row = 1, column = 0, padx = 30, pady = 10)
+entIntRate = Entry(main, textvariable = b).grid(row = 1, column = 1)
+
+btn = Button(main, text = 'Download Completed', command=Download).grid(row = 5, column = 1)
+#download=Download()
+#lblMonthly = Label(main, text =  % download).grid(row = 3, column = 0, padx = 0, pady = 10)
+main.mainloop()
